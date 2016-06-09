749557f9d94c4247e533164d6a6cbffc94f1e8f9
xiaoyao <xiaoyao@yahoo-inc.com>
2016-06-08 19:25:50 -0500

    fix multiplex transaction tests

--- elide-core/src/test/java/com/yahoo/elide/core/PersistentResourceTest.java
+++ elide-core/src/test/java/com/yahoo/elide/core/PersistentResourceTest.java
@@ -44,7 +44,6 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 
 import org.mockito.Answers;
-import org.mockito.Matchers;
 import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -1259,7 +1258,7 @@ public class PersistentResourceTest extends PersistentResource {
         DataStoreTransaction tx = mock(DataStoreTransaction.class, Answers.CALLS_REAL_METHODS);
         User goodUser = new User(1);
 
-        when(tx.loadObjects(eq(Child.class), Matchers.any(FilterScope.class)))
+        when(tx.loadObjects(eq(Child.class), any(FilterScope.class)))
                 .thenReturn(Lists.newArrayList(child1, child2, child3, child4, child5));
 
         RequestScope goodScope = new RequestScope(null, tx, goodUser, dictionary, null, MOCK_AUDIT_LOGGER);
--- elide-datastore/elide-datastore-multiplex/src/test/java/com/yahoo/elide/datastores/multiplex/MultiplexTransactionTest.java
+++ elide-datastore/elide-datastore-multiplex/src/test/java/com/yahoo/elide/datastores/multiplex/MultiplexTransactionTest.java
@@ -5,13 +5,15 @@
  */
 package com.yahoo.elide.datastores.multiplex;
 
-import com.yahoo.elide.core.DataStore;
-import com.yahoo.elide.core.DataStoreTransaction;
-import org.testng.annotations.Test;
-
+import static org.mockito.Matchers.any;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
+
+import com.yahoo.elide.core.DataStore;
+import com.yahoo.elide.core.DataStoreTransaction;
+
+import org.testng.annotations.Test;
 
 /**
  * Tests MultiplexTransaction
@@ -24,8 +26,8 @@ public class MultiplexTransactionTest {
         DataStoreTransaction tx1 = mock(DataStoreTransaction.class);
         DataStoreTransaction tx2 = mock(DataStoreTransaction.class);
 
-        when(store1.beginReadTransaction()).thenReturn(tx1);
-        when(store2.beginReadTransaction()).thenReturn(tx2);
+        when(store1.beginReadTransaction(any())).thenReturn(tx1);
+        when(store2.beginReadTransaction(any())).thenReturn(tx2);
 
         MultiplexManager store = new MultiplexManager(store1, store2);
 
